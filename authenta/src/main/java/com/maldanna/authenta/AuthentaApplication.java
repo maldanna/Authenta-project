46138d1d34efa8b8e75ee3d1eadcd6c148fcf568
km-mnk <maldanna432432@gmail.com>
2022-10-01 18:04:26 +0000

    project setup

---
+++ authenta/src/main/java/com/maldanna/authenta/AuthentaApplication.java
@@ -0,0 +1,13 @@
+package com.maldanna.authenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentaApplication.class, args);
+	}
+
+}
---
+++ authenta/src/test/java/com/maldanna/authenta/AuthentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maldanna.authenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
